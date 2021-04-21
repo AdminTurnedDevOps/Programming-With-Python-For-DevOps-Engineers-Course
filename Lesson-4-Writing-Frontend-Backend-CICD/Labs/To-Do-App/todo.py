4f113049e6d309d5f4c3324a469d0fdae6a787e0
Michael Levan <mlevan1992@gmail.com>
2021-04-21 10:35:40 -0400

    To-do

--- Lesson-4-Writing-Frontend-Backend-CICD/Labs/To-Do-App/todo.py
+++ Lesson-4-Writing-Frontend-Backend-CICD/Labs/To-Do-App/todo.py
@@ -1,4 +1,3 @@
-# tkinter allows you to create GUI apps with Python
 from tkinter import *
 from tkinter.font import Font
 
@@ -8,6 +7,7 @@ app.title('CloudSkills To-Do')
 app.geometry("500x500")
 
 # Font
+
 font = Font(
     family="Brush Script MT",
     size=30,
@@ -15,50 +15,36 @@ font = Font(
 )
 
 frame = Frame(app)
-# Push down the GUI screen a little bit
 frame.pack(pady=10)
 
-# What the box for the to-do app looks like
-list1 = Listbox(frame,
-                width=40,
-                height=10,
-                # Background border
-                bg="SystemButtonFace",
-                # Borderline
-                bd=0,
-                # Font color
-                fg="#464646")
+list1 = Listbox(
+    frame,
+    width=40,
+    height=10,
+    bg="SystemButtonFace",
+    bd=0,
+    fg="#464646"
+)
 
-# Pack into under the screen
 list1.pack(side=LEFT, fill=BOTH)
 
-# Make a list of stuff to do
-my_stuff = ["Dog", "pick up son from daycare", "deep work"]
-
-# Save it to the list
+my_stuff = ["Pick up dog", "Pick up son from daycare", "deep work"]
 for item in my_stuff:
     list1.insert(END, item)
 
 
-# Scrollabar
 scrollbar = Scrollbar(frame)
 scrollbar.pack(side=RIGHT, fill=BOTH)
 
-# Add scrollbar to list config
 list1.config(yscrollcommand=scrollbar.set)
 
-# Configure scrollbar
 scrollbar.config(command=list1.yview)
 
-# Create an entrybox
 entry = Entry(app)
 entry.pack(pady=20)
 
-# Create a button frame
 button_frame = Frame(app)
 button_frame.pack(pady=20)
-
-# Functions to delete, add, and cross of items on the list
 
 def delete_item():
     list1.delete(ANCHOR)
@@ -67,12 +53,11 @@ def add_item():
     list1.insert(END, entry.get())
     entry.delete(0, END)
 
-
-# Add buttons
 delete_button = Button(button_frame, text="Delete Item", command=delete_item)
 add_button = Button(button_frame, text="Add Item", command=add_item)
 
 delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1)
+
 
 app.mainloop()
