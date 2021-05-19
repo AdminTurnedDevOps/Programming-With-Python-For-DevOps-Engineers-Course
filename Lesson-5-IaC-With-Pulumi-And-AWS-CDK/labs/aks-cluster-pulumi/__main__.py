1f9bc35745fa56d337e7dd1d4d68565ed8f6aff6
Michael Levan <mlevan1992@gmail.com>
2021-05-19 11:54:08 -0400

    less 5

--- Lesson-5-IaC-With-Pulumi-And-AWS-CDK/labs/aks-cluster-pulumi/__main__.py
+++ Lesson-5-IaC-With-Pulumi-And-AWS-CDK/labs/aks-cluster-pulumi/__main__.py
@@ -3,15 +3,15 @@ from pulumi_azure import containerservice, core
 
 
 containerservice.KubernetesCluster(
-    resource_name='cloudskillsaks',
+    resource_name='cloudskillsakscluster',
     default_node_group={
-        'min_count': 1,
-        'max_count': 1,
-        'name': 'cloudskillsaks',
+        'min_count': 2,
+        'max_count': 10,
+        'name': 'cloudskillsakscluster',
         'vm_size': 'Standard_D12_v2',
         'enable_auto_scaling': True
     },
-    dns_prefix='cloudskillsdns',
+    dns_prefix='mynewdnscloudskills',
     resource_group_name='cloudskills-rg',
     service_principal={
         'client_id': '',
